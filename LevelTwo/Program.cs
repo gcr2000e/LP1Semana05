a1bdbf1503472b90bd1d4b57ec6ffe7219ad96d7
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-20 15:24:57 +0000

    Add LevelTwo new code

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SixLabors.ImageSharp.Formats.Gif;
 using Spectre.Console;
 
 namespace LevelTwo
@@ -8,8 +9,23 @@ namespace LevelTwo
         private static void Main(string[] args)
         {
             CanvasImage image = new CanvasImage("tux.jpg");
-            image.MaxWidth(20);
-            AnsiConsole.Write(image);
+            image = image.MaxWidth(24);
+            string randomImage = "";
+            string n = "";
+            int Width = int.Parse(n);
+
+            if (args.Length >= 2)
+            {
+                CanvasImage image2 = new CanvasImage(randomImage);
+                image2.MaxWidth(Width);
+                AnsiConsole.Write(image2);
+                return;
+            }
+            else if (args.Length == 0)
+            {
+                AnsiConsole.Write(image);
+                return;
+            }
         }
     }
 }
