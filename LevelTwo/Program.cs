78e1aa976e917a0c9bf6e8021cf1632dc656049c
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-20 10:42:39 +0000

    LevelTwo

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CanvasImage image = new CanvasImage("tux.jpg");
+            image.MaxWidth(40);
+            AnsiConsole.Write(image);
+        }
+    }
+}
