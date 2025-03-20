c8a29795889d018de163aa54e3d7c77ae2a7814e
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-20 09:47:50 +0000

    Add method doc

---
+++ LevelOne/MainIsHere.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LevelOne
+{
+    public class MainIsHere
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
