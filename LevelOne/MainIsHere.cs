25957a22c3ebaa344ffd42ef44906f237e4edda7
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-20 10:05:35 +0000

    Fixed code

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -2,15 +2,17 @@
 
 namespace LevelOne
 {
+<<<<<<< HEAD
     public class MainIsHere
     {
         /// <summary>
         /// Main is here!
         /// </summary>
         /// <param name="args"></param>
+=======
+>>>>>>> testing
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Nice Day!");
         }
-    }
 }
